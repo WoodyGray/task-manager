1b44b84dff0ec4acda73cb05d58badc9292e4eaf
WoodyGray <115577474+WoodyGray@users.noreply.github.com>
2023-10-19 16:39:26 +0300

    commit

---
+++ server/src/main/java/com/woody/task_manager/security/JwtTokenRepository.java
@@ -0,0 +1,66 @@
+package com.woody.task_manager.security;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.DefaultCsrfToken;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS;
+
+public class JwtTokenRepository implements CsrfTokenRepository {
+    private String secret;
+
+    public JwtTokenRepository(){
+        this.secret = "springrest";
+    }
+
+    @Override
+    public CsrfToken generateToken(HttpServletRequest request) {
+        String id = UUID.randomUUID().toString().replace("-", "");
+        Date now = new Date();
+        Date exp = Date.from(LocalDateTime.now().plusMinutes(30)
+                .atZone(ZoneId.systemDefault()).toInstant());
+
+        String token = "";
+        try{
+            token = Jwts.builder()
+                    .setId(id)
+                    .setIssuedAt(now)
+                    .setNotBefore(now)
+                    .setExpiration(exp)
+                    .signWith(SignatureAlgorithm.ES256, secret)
+                    .compact();
+        } catch (JwtException e) {
+            e.printStackTrace();
+        }
+        return new DefaultCsrfToken("x-csrf-token", "_csrf", token);
+    }
+
+    @Override
+    public void saveToken(CsrfToken csrfToken, HttpServletRequest request, HttpServletResponse response) {
+        if (Objects.nonNull(csrfToken)){
+            if(!response.getHeaderNames().contains(ACCESS_CONTROL_EXPOSE_HEADERS))
+                response.addHeader(ACCESS_CONTROL_EXPOSE_HEADERS, csrfToken.getHeaderName());
+
+            if (response.getHeaderNames().contains(csrfToken.getHeaderName()))
+                response.setHeader(csrfToken.getHeaderName(), csrfToken.getToken());
+            else
+                response.addHeader(csrfToken.getHeaderName(), csrfToken.getToken());
+        }
+    }
+
+    @Override
+    public CsrfToken loadToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+    }
+}
